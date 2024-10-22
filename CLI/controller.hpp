9595c8f903c6e1b8c52960b9209f53a06f2f0f53
Tigran Khachakranc <tigrankhachakranc@users.noreply.github.com>
2024-10-22 17:32:24 +0400

    Update controller.hpp

--- CLI/controller.hpp
+++ CLI/controller.hpp
@@ -6,6 +6,6 @@ class Controller
     Parser parser; // composition
 
 public:
-    bool m_isRunning = true;
+    bool m_isRunning = true; //TK: This is bad, members should not be public, you should have a method exit()
     void run(std::istream &input);
 };
